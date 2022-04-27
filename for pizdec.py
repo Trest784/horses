8a9b291f4170cfcf5772a08a45640256d69489e1
Trest784 <101951346+Trest784@users.noreply.github.com>
2022-04-28 01:08:06 +0300

    Add files via upload

---
+++ for pizdec.py
@@ -0,0 +1,23 @@
+n=int(input("Enter the number"))
+factorial = 1
+for i in range(2,n+1):
+    fact=factorial * i
+print(fact)
+
+
+n=9
+fact=1
+for i in range(2,n+1):
+    fact=fact*i
+print("The factorial of",n,"is:",fact)
+
+n = 3
+fact = 1
+for i in range(4):
+    factorial = fact * i
+    print(factorial)
+
+
+
+
+
---
+++ sensei home work while.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the number"))
+while n > 0:
+    print("n")
+else: n = 0
+print("на этом все")
