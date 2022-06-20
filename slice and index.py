0acdd6d2c8672e2be69c66f5842a9e1127f71e54
razmanov666 <razmanov666@gmail.com>
2022-06-20 11:02:52 +0300

    add comments for mistakes

--- slice and index.py
+++ slice and index.py
@@ -2,23 +2,25 @@
 #1 вырезать первые пять данных
 
 list_a = [2,3,4,5,6,7,8,9,1,2,6,72,432,535,46,76,68,34]
-list_ab = slice(6)
-print(list_a[list_ab])
+list_ab = slice(6) # Alexey: почему не list_a[:5]?
+# print(list_a[list_ab])
 
 #nice
 
 #2 все четные до 6
 
 list_b = [0,1,2,3,4,5,6,7,8,9,10]
-x = slice(0,6,2)
-print(list_b[x])
+x = slice(0,6,2) # Alexey: list_b[0:7:2]
+# print(list_b[x])
 
 #3 несколько элементов из нескольких списков
 #
-# list_b = [0,1,2,3,4,5,6,7,[23,23,3,44,56,77,12]]
+list_b = [0,1,2,3,4,5,6,7,[23,23,3,44,56,77,12]]
 # x[0] = slice(1,4)
+x = list_b[:3]
 # y[1] = slice(2)
-# print(list_b[x,y])
+# y = slice(2)
+# print(x)
 
 # заебало что с синтаксисом почему такая проблема с синтаксисиом у слайсинга с двойным списком
 
@@ -34,7 +36,7 @@ list_b = ["nevill","denisov","raul"]
 y = list_b[0][0]
 c = list_b[1][0]
 e = list_b[2][0]
-print(y,c,e)
+# print(y,c,e)
 
 #  БЛЯ ДОПЕР ДО СИНТАКИСИА ВСЕ - ОЧЕНЬ ДОЛГО ЩАС КОВЫРЯЛСЯ ПИЗДОС
 
@@ -44,19 +46,19 @@ list_c = ['bremen','union','inter',]
 b = list_c[0][-1]
 u = list_c[1][-1]
 i = list_c[2][2:]
-print(b,u,i)
+# print(b,u,i)
 
 # list_index
 
 list_b = [2,3,4,5,6,7,8,9,10,12,3,3,5435646,2423,76578,1]
 index = list_b.index(2,0,3)
-print(index)
+# print(index)
 
 
 list_v = [6,5,4,3,2,6,7,89,0,123,343,46578,68,90,34,4,00,998,7,65,44,33,21]
 index = list_v.index(33)
 index1 = list_v.index(2)
-print(index,index1)
+# print(index,index1)
 
 #
 #
