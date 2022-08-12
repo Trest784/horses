2dd1d0bf28c1e31a34b60cdabdbfe7a0ac8d8129
Andrey <tryboy784@yandex.ru>
2022-08-12 23:40:42 +0300

    не дотянул концентрат 2

---
+++ night work after lection.py
@@ -0,0 +1,20 @@
+
+asd = [1,2,3,4,5,6,7,8,9,10]
+bas = ['verder','psv','arsenal','sevilla']
+
+def control(var,arm):
+    var = united
+    arm = quantity
+
+def united(cls):
+   cls = asd + bas
+   return cls
+
+
+def quantity(btb):
+    btb = cls[0:-1]
+    return btb
+
+
+control(asd,bas)
+print(control)
